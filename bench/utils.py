e7d9bfe8d1ad538a4aa41f7fb284d1f2a3b6ed03
jvfiel <jvfiel@bai.ph>
2017-12-19 21:56:09 +0800

    https://github.com/jvfiel/erpnext-v9.git

--- bench/utils.py
+++ bench/utils.py
@@ -19,7 +19,7 @@ def get_frappe(bench_path='.'):
 	frappe = get_env_cmd('frappe', bench_path=bench_path)
 	if not os.path.exists(frappe):
 		print('frappe app is not installed. Run the following command to install frappe')
-		print('bench get-app https://github.com/frappe/frappe.git')
+		print('bench get-app https://github.com/jvfiel/frappe-v9.git')
 	return frappe
 
 def get_env_cmd(cmd, bench_path='.'):
@@ -59,7 +59,7 @@ def init(path, apps_path=None, no_procfile=False, no_backups=False,
 		clone_apps_from(bench_path=path, clone_from=clone_from)
 	else:
 		if not frappe_path:
-			frappe_path = 'https://github.com/frappe/frappe.git'
+			frappe_path = 'https://github.com/jvfiel/frappe-v9.git'
 
 		get_app(frappe_path, branch=frappe_branch, bench_path=path, build_asset_files=False, verbose=verbose)
 
